638b4182242d981670c783248de56b2bc65135d9
William Holcombe <sphinxelite@Williams-iMac.local>
2018-11-12 22:43:56 -0500

    yoga setup

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,15 @@
+//this file connects to the remote prisma db and gives us the ability to query the db
+
+const { Prisma } = require("prisma-binding");
+
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
+//4min  12vid
+
+//only turn dbug on if needed
