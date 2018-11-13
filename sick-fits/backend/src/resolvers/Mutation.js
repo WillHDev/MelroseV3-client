10623d8c7d84fa1c21267a7bcf6f573fef43ee47
William Holcombe <sphinxelite@Williams-iMac.local>
2018-11-13 13:46:45 -0500

    finished main backend setup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,29 @@
 const mutations = {
-  createDog(parent, args, ctx, info) {
-    //create a dog!
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    //TODO: check if they are logged in
+    //how we access the db couldve imported it
+    //return ctx... promise instead of
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    console.log(item);
+    return item;
   }
 };
 
 module.exports = mutations;
+
+//  title: args.title,
+
+// createDog(parent, args, ctx, info) {
+//   //create a dog!
+//   global.dogs = global.dogs || [];
+//   const newDog = { name: args.name };
+//   global.dogs.push(newDog);
+//   return newDog;
+// }
